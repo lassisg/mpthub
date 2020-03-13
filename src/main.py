5603c4600a7d5b35a26443e168e84e6867bd6050
Leandro Gabriel <lassisg@gmail.com>
2020-03-13 01:45:00 +0000

    Config.db now lives under user directory.

--- src/main.py
+++ src/main.py
@@ -5,14 +5,19 @@ from mpt.mpt import Analysis
 from mpt.database import Database
 import os
 import time
+from pathlib import Path, PurePath
 
 # Initial setup
 CFG_PATH = os.path.join(os.path.dirname(
     os.path.realpath(__file__)), 'data')
-DB_PATH = os.path.join(CFG_PATH, 'config.db')
 
 
 def main():
+    if not PurePath.joinpath(Path.home(), '.mpt').exists():
+        Path.mkdir(PurePath.joinpath(Path.home(), '.mpt'))
+
+    DB_PATH = str(PurePath.joinpath(Path.home(), '.mpt', 'config.db'))
+
     # app = mptApp()
     # app = wx.App()
     # frame = Main()
@@ -22,13 +27,13 @@ def main():
     db = Database(DB_PATH)
     db.persist()
 
-    # TODO: Code below must go to GUI
-    analysis = Analysis(CFG_PATH)
-    analysis.load_config(db)
-    analysis.add_report()
-    if analysis.report_list:
-        analysis.analyze()
-        analysis.export()
+    # # TODO: Code below must go to GUI
+    # analysis = Analysis(CFG_PATH)
+    # analysis.load_config(db)
+    # analysis.add_report()
+    # if analysis.report_list:
+    #     analysis.analyze()
+    #     analysis.export()
 
     db.conn.close()
 
@@ -38,3 +43,4 @@ if __name__ == '__main__':
     main()
     # end_time = time.time()
     # print(f"Elapsed time: {end_time-start_time}")
+    # home = str(Path.home())
