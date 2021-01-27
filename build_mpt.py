de209ec1bd0c9beb244fcba09146b0600ebf595a
Leandro Gabriel <lassisg@gmail.com>
2021-01-27 15:45:04 +0000

    Added a script to make it easy to build the app.

---
+++ build_mpt.py
@@ -0,0 +1,16 @@
+import subprocess
+
+
+def main():
+    data_options = [
+        'README.md;.',
+        'LICENSE;.',
+        'mpt/assets/*;./mpt/assets/'
+    ]
+    app_name = 'MPT_Analysis'
+    icon_path = './mpt/assets/icon.ico'
+    subprocess.run(f"python -m PyInstaller --clean -D --noconfirm -w \
+                    --add-data={data_options[0]} \
+                    --add-data={data_options[1]} \
+                    --add-data={data_options[2]} \
+                    -n {app_name} -i {icon_path} app.py")
