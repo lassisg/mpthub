a2463a5b083a6de21dfe1292fa0cf99877c2414a
Leandro Gabriel <lassisg@gmail.com>
2021-01-29 08:25:40 +0000

    Code legibility improved.

--- build_mpt.py
+++ build_mpt.py
@@ -5,10 +5,10 @@ def main():
     data_options = [
         'README.md;.',
         'LICENSE;.',
-        'mpt/assets/*;./mpt/assets/'
-    ]
+        'mpt/assets/*;./mpt/assets/']
     app_name = 'MPT_Analysis'
     icon_path = './mpt/assets/icon.ico'
+
     subprocess.run(f"python -m PyInstaller --clean -D --noconfirm -w \
                     --add-data={data_options[0]} \
                     --add-data={data_options[1]} \
