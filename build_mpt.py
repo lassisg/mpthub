f558d6c30ecfada2b1df30bd8625033b83201fe2
Leandro Gabriel <lassisg@gmail.com>
2021-02-05 19:41:08 +0000

    Added hooks to allow trackpy to be packed with the app.

--- build_mpt.py
+++ build_mpt.py
@@ -6,11 +6,18 @@ def main():
         'README.md;.',
         'LICENSE;.',
         'mpt/assets/*;./mpt/assets/']
+    hidden = [
+        'scipy.spatial.transform._rotation_groups',
+        'scipy.special.cython_special'
+    ]
     app_name = 'MPT_Analysis'
     icon_path = './mpt/assets/icon.ico'
 
+    # subprocess.run(f"python -m PyInstaller --clean -D --noconfirm \
     subprocess.run(f"python -m PyInstaller --clean -D --noconfirm -w \
                     --add-data={data_options[0]} \
                     --add-data={data_options[1]} \
                     --add-data={data_options[2]} \
+                    --hidden-import={hidden[0]} \
+                    --hidden-import={hidden[1]} \
                     -n {app_name} -i {icon_path} app.py")
