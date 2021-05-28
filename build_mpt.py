248cbcd3c48724f8b911802f4017e5c3cba3405b
Leandro Gabriel <lassisg@gmail.com>
2021-05-28 14:23:11 +0100

    Build script adjustments done.

--- build_mpt.py
+++ build_mpt.py
@@ -1,20 +1,19 @@
 import subprocess
 from os.path import join as path_join
-from os import environ as read_env
+from os import sep
 
 
 def main():
-    env_path = "D:/virtualenvs/mpt-CrFBuUTO-py3.8/"
-    paths = f'{env_path}Lib/site-packages'
-    gui_platform_path = path_join(read_env.get('PROGRAMFILES(X86)'),
-                                  "Qt Designer", 'platforms', '*')
+    env_path = "D:/virtualenvs/mpt-CrFBuUTO-py3.8"
+    paths = f'{env_path}/Lib/site-packages'
+
     data_options = [
         'README.md;.',
         'LICENSE;.',
         'mpt/assets/*;./mpt/assets/',
         'ui/*;./ui/',
-        # f'{gui_platform_path};./platforms/'
-        f'{env_path}Lib/site-packages/PySide6/plugins/platforms/*;./platforms/'
+        # 'platforms/*;./platforms/'
+        # f'{paths}/PySide6/plugins/platforms/*;./platforms/'
     ]
     # The following hidden-import were added only because of TrackPy
     hidden = [
@@ -28,16 +27,24 @@ def main():
     icon_path = './ui/resources/icons/node-magnifier.ico'
 
     # subprocess.run(f"python -m PyInstaller --clean -D --noconfirm \
-    # --debug=all \
+    #                 --paths={path_libs} \
+    #                 --debug=all \
+    #                 --debug=imports \
+    #                 --add-data={data_options[4]} \
     subprocess.run(f"python -m PyInstaller --clean -D --noconfirm -w \
                     --paths={paths} \
                     --add-data={data_options[0]} \
                     --add-data={data_options[1]} \
                     --add-data={data_options[2]} \
                     --add-data={data_options[3]} \
-                    --add-data={data_options[4]} \
                     --hidden-import={hidden[0]} \
                     --hidden-import={hidden[1]} \
                     --hidden-import={hidden[2]} \
                     --hidden-import={hidden[3]} \
-                    -n {app_name} -i {icon_path} app.py")
+                    -n {app_name} \
+                    -i {icon_path} \
+                    app.py")
+
+
+if __name__ == '__main__':
+    main()
