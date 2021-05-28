fea738796d783d71a356259d91919903072620e7
Leandro Gabriel <lassisg@gmail.com>
2021-05-28 14:19:11 +0100

    Old files removed.

--- remove/app.py
+++
@@ -1,8 +0,0 @@
-import wx
-from mpt.views import mainWindow
-
-if __name__ == "__main__":
-    mpt_app = wx.App()
-    frame = mainWindow(parent=None)
-    frame.Show(True)
-    mpt_app.MainLoop()
--- remove/mpt/__init__.py
+++
@@ -1,9 +0,0 @@
-__version__ = "0.7.1"
-
-from .model import Analysis, Diffusivity, General
-import mpt.database as db
-
-db.persist()
-analysis = Analysis()
-diffusivity = Diffusivity()
-general = General()
