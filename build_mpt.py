b25bb0b5f6f3d20f32f04a56455ebe9e2804a1a5
Leandro Gabriel <lassisg@pm.me>
2021-07-30 13:12:03 +0100

    Built script improvements.

--- build_mpt.py
+++ build_mpt.py
@@ -1,20 +1,22 @@
 import subprocess
 from os.path import join as path_join
-from os import sep
+import sys
 
 
 def main():
-    env_path = "D:/virtualenvs/mpt-CrFBuUTO-py3.8"
+    # App related information
+    app_name = 'MPTHub'
+    icon_path = './ui/resources/icons/node-magnifier.ico'
+    env_path = sys.exec_prefix
     paths = f'{env_path}/Lib/site-packages'
-
     data_options = [
         'README.md;.',
         'LICENSE;.',
         'mpt/assets/*;./mpt/assets/',
         'ui/*;./ui/',
-        # 'platforms/*;./platforms/'
         f'{paths}/PySide6/plugins/platforms/*;./platforms/'
     ]
+
     # The following hidden-import were added only because of TrackPy
     hidden = [
         'pandas',
@@ -22,15 +24,18 @@ def main():
         'scipy.spatial.transform._rotation_groups',
         'scipy.special.cython_special'
     ]
-    app_name = 'MPT_Hub'
-    # icon_path = './mpt/assets/icon.ico'
-    icon_path = './ui/resources/icons/node-magnifier.ico'
 
     # subprocess.run(f"python -m PyInstaller --clean -D --noconfirm \
     #                 --paths={path_libs} \
     #                 --debug=all \
     #                 --debug=imports \
-    subprocess.run(f"python -m PyInstaller --clean -D --noconfirm -w \
+    #                 --log-level DEBUG \
+
+    # Added '--exclude-module matplotlib' due to build errors
+    # Solution from https://rb.gy/qgabep
+    subprocess.run(f"{sys.executable} -m PyInstaller \
+                    --log-level WARN \
+                    --clean -D --noconfirm -w \
                     --paths={paths} \
                     --add-data={data_options[0]} \
                     --add-data={data_options[1]} \
@@ -41,6 +46,7 @@ def main():
                     --hidden-import={hidden[1]} \
                     --hidden-import={hidden[2]} \
                     --hidden-import={hidden[3]} \
+                    --exclude-module matplotlib \
                     -n {app_name} \
                     -i {icon_path} \
                     app.py")
