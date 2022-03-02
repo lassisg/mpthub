b1d1407ff28330c171ecd08ae44d1e1f3d224206
Leandro Gabriel <leandro@bigiogabriel.com>
2022-03-02 10:06:54 +0000

    Changes D0 to Deff in MS Excel sheet Fixes #27

--- mpt/model.py
+++ mpt/model.py
@@ -1035,7 +1035,7 @@ class Report():
         worksheet.merge_range('H1:H2', '', summary_format)
         worksheet.write_rich_string('H1',
                                     summary_format, 'D',
-                                    subscript_format, '0\n',
+                                    subscript_format, 'eff\n',
                                     summary_format, f'({chr(956)}m',
                                     superscript_format, '2',
                                     summary_format, '-s',
@@ -1046,7 +1046,7 @@ class Report():
         worksheet.merge_range('I1:I2', '', summary_format)
         worksheet.write_rich_string('I1',
                                     summary_format, 'D',
-                                    subscript_format, '0\n',
+                                    subscript_format, 'eff\n',
                                     summary_format, '(m',
                                     superscript_format, '2',
                                     summary_format, '-s',
