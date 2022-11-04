e0be21ae1b9aa85189d36d45cf231878a4dfee22
Leandro Gabriel <leandro@bigiogabriel.com>
2022-11-04 12:09:18 +0000

    Fixes merge content

--- mpt/__init__.py
+++ mpt/__init__.py
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 __version__ = "1.0.4"
-=======
-__version__ = "1.0.3"
->>>>>>> 00d05b3178954d97368122f4ffa615fc232460fc
 
 from .model import Analysis, Diffusivity, General
 import mpt.database as db
